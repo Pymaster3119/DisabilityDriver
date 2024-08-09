3ba7d9f16d81c1ffbfe1747617a6c43b0a7d6b54
pymaster9 <adi31tya@icloud.com>
2024-08-09 10:03:54 -0400

    Improving Speech To Text

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,19 +1,26 @@
 import speech_recognition as sr
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
+import threading
+def process(audio, recognizer):
+    try:
+        text = recognizer.recognize_google(audio)
+        displayCaption(text)
+    except sr.UnknownValueError:
+        pass
 
-with microphone as source:
-    print("Adjusting for ambient noise... Please wait.")
-    recognizer.adjust_for_ambient_noise(source)
-    print("Listening...")
+    
+def live_caption():
+    recognizer = sr.Recognizer()
+    recognizer.pause_threshold = 0.5
+    recognizer.dynamic_energy_threshold = True
+    with sr.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic)
+        while True:
+            try:
+                audio = recognizer.listen(mic, timeout=100000,phrase_time_limit=1)
+                threading.Thread(target=lambda: process(audio, recognizer)).start()
+            except Exception as e:
+                print(e)
 
-    while True:
-        print("Listening for input...")
-        audio = recognizer.listen(source)
-        try:
-            text = recognizer.recognize_google(audio)
-            print(text)
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+#Filler function for stuff in the UI
+def displayCaption(caption):
+    print(caption)
