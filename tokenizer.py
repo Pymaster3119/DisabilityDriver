00de5e168040d542e785c19cc67ec0926509f214
pymaster9 <adi31tya@icloud.com>
2024-08-22 22:05:31 -0400

    Searching more intelligently through children to save money!!!

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import tiktoken
 enc = tiktoken.get_encoding("o200k_base")
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
+def num_tokens_from_string(string: str, encoding_name: str = "o200k_base") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
--- treesearch.py
+++ treesearch.py
@@ -1,19 +1,18 @@
 import queryGPT
 from bs4 import *
+import tokenizer
 
 elements = []
 with open("treesearchsystemtext", "r") as txt:
     systemtext = txt.read()
 
 def removeattributes(tag):
-    
     for tag in tag.find_all(True):
         attrs = tag.attrs
         if 'id' in attrs:
             tag.attrs = {'id': attrs['id']}
         else:
             tag.attrs = {}
-    
     return str(tag)
 
 def decideonelement(element,prompt):
@@ -42,8 +41,36 @@ def removeads(soup):
         ad.decompose()
     return soup
 
+def findtokens(element, depth=0, resultlist=None):
+    if resultlist is None:
+        resultlist = []
+    if depth >= len(resultlist):
+        resultlist.append([])
+    if element.name:
+        result = tokenizer.num_tokens_from_string(element)
+        resultlist[depth].append(result)
+    for child in element.children:
+        if child.name:
+            findtokens(child, depth + 1, resultlist)
+    
+    return resultlist
+elementsofinterest = ['p', 'button', 'form']
+def findscores(element, depth=0, scorelist=None):
+    if scorelist is None:
+        scorelist = []
+    if depth >= len(scorelist):
+        scorelist.append(0)
+    if element.name in elementsofinterest:
+        scorelist[depth] += 1
+    for child in element.children:
+        if child.name:
+            findscores(child, depth + 1, scorelist)
+    
+    return scorelist
+    
+
 def treesearch(html, prompt):
-    problem += "Select relevant fields, links and buttons necessary to fullfil thd prompt provided."
+    problem += "Select relevant fields, links and buttons necessary to fullfil the user's prompt. Remember to keep as little HTML as possible while still making the user's prompt possible based on the trimmed HTML you produce alone."
     if html.strip().lower().startswith('<!doctype'):
         html = html.split('>', 1)[1]
     soup = BeautifulSoup(html, 'html.parser')
@@ -53,5 +80,11 @@ def treesearch(html, prompt):
             if isinstance(elem, Tag):
                     elem.decompose()
     soup = removeads(soup)
+    '''
     for i in soup.children:
-        decideonelement(soup, prompt)
+        decideonelement(soup, prompt)'''
+    
+    #One subtree for each child to body
+    for subtree in soup.find("body").children:
+        tokens = findtokens(subtree)
+        scores = findscores(subtree)
